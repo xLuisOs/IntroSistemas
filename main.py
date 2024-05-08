7a35ac39be415f2b1edd2257de61de9ee87acc18
Luis Granados <luisoswaldogra@gmail.com>
2024-05-08 09:17:33 -0600

    Datos de la U en main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Nombre: Luis Oswaldo Granados de León")
 print("Carnet: 1506124")
 print("Curso: Introduccion a la Ingenieria en Informatica  y Sistemas")
 print("Ing. Miguel Matul")
+
+print("Universidad Rafael Landívar")
+print("Campus San Alberto Hurtado SJ")
